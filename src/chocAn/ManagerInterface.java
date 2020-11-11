31dda6f472f85ab05090b8e60a7b7ff6bb27019b
dldillard <dldillard@crimson.ua.edu>
2020-11-11 17:09:07 -0600

    Updating ManagerInterface by adding functionality

--- src/chocAn/ManagerInterface.java
+++ src/chocAn/ManagerInterface.java
@@ -1,35 +1,55 @@
 package chocAn;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+
 public class ManagerInterface {
-	//private GenerateReports gen;
 	
-	public ManagerInterface(){
-		//gen = new GenerateReports();
-	}
 	
 	public void prompt(){
 		
 	}
 	
-	public MemberReport requestMemberReport(){
-		MemberReport mrep = new MemberReport();
-		mrep = GenerateReports.generateMemberReport(number);
+	public MemberReport requestMemberReport() {
 		
-		return mrep;
+		GenerateReports generateReport = new GenerateReports();
+		
+		String memberNumber = JOptionPane.showInputDialog("What is the identification number of the member you wish to run the record of?");
+		MemberReport memberReport = generateReport.generateMemberReport(memberNumber);	
+		
+		memberReport.printReport();
+		return memberReport;
+	
 	}
 	
-	public ProviderReport requestProviderReport(){
-		ProviderReport prep = new ProviderReport();
-		prep = GenerateReports.generateProviderReport(number);
+	public ProviderReport requestProviderReport() {
+		GenerateReports generateReport = new GenerateReports();
 		
-		return prep;
+		String providerNumber = JOptionPane.showInputDialog("What is the identification number of the provider you wish to run the record of?");		
+		ProviderReport providerReport = generateReport.generateProviderReport(providerNumber);	
+		
+		providerReport.printReport();
+		return providerReport;
+	
 	}
 	
-	public SummaryReport requestSummaryReport(){
-		SummaryReport srep = new SummaryReport();
-		srep = GenerateReports.generateSummaryReport(services);
+	public SummaryReport requestSummaryReport() {
 		
-		return srep;
+		GenerateReports generateReport = new GenerateReports();
+		
+		Provider grandProvider = new Provider();
+		List<Provider> providers = grandProvider.getProvidersFromDatabase();
+		Service grandService = new Service();
+		List<Service> weekServices = new ArrayList<Service>();
+		for(Provider provider: providers) {
+			String number = provider.getNumber();
+			weekServices.addAll(grandService.getProviderServices(number));
+		}
+		SummaryReport summaryReport = generateReport.generateSummaryReport(weekServices);
+		summaryReport.printReport();
+		return summaryReport;
+	
 	}
 	
 	public EftData requestEftData(){
