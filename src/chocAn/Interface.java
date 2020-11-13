e40090a292ff7504555b68fa5ab7e7bdac0b2d36
JGrin <jgrinstead@crimson.ua.edu>
2020-11-13 16:01:52 -0600

    Quick push so my fellows have access to "choclate.png"

--- src/chocAn/Interface.java
+++ src/chocAn/Interface.java
@@ -1,5 +1,7 @@
 package chocAn;
 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Interface {
@@ -8,10 +10,11 @@ public class Interface {
 	private OperatorInterface operator;
 	
 	public static void main(String[] args){
-		
+		prompt();
 	}
 	
-	public void prompt(){
+	public static void prompt(){
+		JFrame frame = new JFrame("ChocAn");
 		JOptionPane.showMessageDialog(frame, "Eggs are not supposed to be green.");
 	}
 }
--- src/chocAn/Manage.java
+++ src/chocAn/Manage.java
@@ -1,59 +1,173 @@
 package chocAn;
 
-import java.util.*;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+//Numbers NEED to be 9
+//Names CANNOT exceed 25 characters
 
 public class Manage {
-	private String err;
 	
 	public Manage(){
-		err="Task Failed";
-	}
-	
-	public void manageMember(){
 		
 	}
 	
+	public void manageMember(){
+		String operation, ret;
+		JFrame frame = new JFrame("ChocAn");
+		ImageIcon icon = new ImageIcon("choclate.png");
+		Object[] possibilities = {"Add Member", "Update Member", "Delete Member"};
+		Object[] possibilities2 = {"Card","Number"};
+		operation = (String)JOptionPane.showInputDialog(frame, "Which operation would you like to perform?","Manage Member",JOptionPane.PLAIN_MESSAGE, icon, possibilities, "Verify Member Card");
+		
+		if (operation == "add"){
+			ret=addMember();
+		}else if (operation == "update"){
+			ret=updateMember();
+		}else{
+			ret=deleteMember();
+		}
+		JOptionPane.showMessageDialog(frame, ret);
+	}
+	
 	public String addMember(){
+		String name, number, address, city, state, zipCode, status;
 		boolean found = false;
-		List<Member> members = Member.getMembersFromDatabase();
-		//code to search list && set boolean
+		//dialogue box required to get info
+		//List<Member> members = Member.getMembersFromDatabase();
+		Member poss = new Member();
+		poss.getMemberByNumber(number);
+		
+		if(poss != null) found = true;
+		
 		if (found){
 			return "This member already exists in the ChocAn database.";
 		}else{
 			Member curr = new Member();
-			
+			curr.setName(name);
+			curr.setNumber(number);
+			curr.setAddress(address);
+			curr.setCity(city);
+			curr.setState(state);
+			curr.setZipCode(zipCode);
+			curr.setStatus(status);
+			curr.addMemberToDatabase();
 			return "The member has been added to the ChocAn database.";
 		}
-		return err;
+		return "Task failed";
 	}
 	
+	//Still needs work
 	public String updateMember(){
-		
-		return err;
+		String name, number, address, city, state, zipCode, status;
+		boolean found = false;
+		//dialogue box required to get info
+		List<Member> members = Member.getMembersFromDatabase();
+		//code to search list && set boolean
+		if (!found){
+			return "This member does not exist in the ChocAn database.";
+		}else{
+			int oldnum = 
+			curr.setName(name);
+			curr.setNumber(number);
+			curr.setAddress(address);
+			curr.setCity(city);
+			curr.setState(state);
+			curr.setZipCode(zipCode);
+			curr.setStatus(status);
+			curr.addMemberToDatabase();
+			return "The member has been updated.";
+		}
+		return "Task failed";
 	}
 	
 	public String deleteMember(){
-		
-		return err;
+		boolean found = false;
+		//dialogue box required to get info
+		List<Member> members = Member.getMembersFromDatabase();
+		//code to search list && set boolean
+		if (!found){
+			return "This member does not exist in the ChocAn database.";
+		}else{
+
+			return "The member has been removed from the ChocAn database.";
+		}
+		return "Task failed";
 	}
 	
 	public void manageProvider(){
+		String operation, ret;
+		//dialogue box required to get info
 		
+		if (operation == "add"){
+			ret=addProvider();
+		}else if (operation == "update"){
+			ret=updateProvider();
+		}else{
+			ret=deleteProvider();
+		}
+		//add a dialogue box that includes message ret
 	}
 	
 	public String addProvider(){
-		
-		return err;
+		String name, number, address, city, state, zipCode;
+		boolean found = false;
+		//dialogue box required to get info
+		List<Provider> providers = Provider.getProvidersFromDatabase();
+		//code to search list && set boolean
+		if (found){
+			return "This provider already exists in the ChocAn database.";
+		}else{
+			Provider curr = new Provider();
+			curr.setName(name);
+			curr.setNumber(number);
+			curr.setAddress(address);
+			curr.setCity(city);
+			curr.setState(state);
+			curr.setZipCode(zipCode);
+			curr.addProviderToDatabase();
+			return "The provider has been added to the ChocAn database.";
+		}
+		return "Task failed";
 	}
 	
 	public String updateProvider(){
-		
-		return err;
+		String name, number, address, city, state, zipCode;
+		boolean found = false;
+		//dialogue box required to get info
+		List<Provider> providers = Provider.getProvidersFromDatabase();
+		//code to search list && set boolean
+		if (found){
+			return "This provider already exists in the ChocAn database.";
+		}else{
+			Provider curr = new Provider();
+			curr.setName(name);
+			curr.setNumber(number);
+			curr.setAddress(address);
+			curr.setCity(city);
+			curr.setState(state);
+			curr.setZipCode(zipCode);
+			curr.addProviderToDatabase();
+			return "The provider has been added to the ChocAn database.";
+		}
+		return "Task failed";
 	}
 	
 	public String deleteProvider(){
-		
-		return err;
+		boolean found = false;
+		//dialogue box required to get info
+		List<Provider> providers = Provider.getProvidersFromDatabase();
+		//code to search list && set boolean
+		if (found){
+			
+			return "This provider has been deleted from the ChocAn database.";
+		}else{
+			return "The provider does not exist in the ChocAn database.";
+		}
+		return "Task failed";
 	}
 
 }
