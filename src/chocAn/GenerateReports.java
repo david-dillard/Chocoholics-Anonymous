42ba677cd5037eb8d742fae9f5568b667c7ee4c2
zmstiles <zmstiles@crimson.ua.edu>
2020-11-30 17:51:29 -0600

    -Updated javadoc for GenerateReports

--- src/chocAn/GenerateReports.java
+++ src/chocAn/GenerateReports.java
@@ -45,9 +45,10 @@ public class GenerateReports {
 	}
 	
 	/**
-	 * constructs and returns an EftData
-	 * @return fully populated EftData
-	 */
+     * constructs and returns an EftData file
+     * @param providerReport a populated Provider Report so that the method can get provider details
+     * @return fully populated EftData
+     */
 	public EftData writeEftData(ProviderReport providerReport) {
 		EftData eftData = new EftData(providerReport.getName(), providerReport.getNumber(), providerReport.getWeeklyFee());
 		return eftData;
